06eb341011173d1f763c28d9ef632159005adc7f
Andrey Usov <nkt.andr@gmail.com>
2020-01-19 01:02:00 +0300

    use ConversationHandler

--- tg-bot.py
+++ tg-bot.py
@@ -3,7 +3,7 @@
 import json
 
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, RegexHandler, ConversationHandler
 import logging
 import redis
 
@@ -13,8 +13,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-question_count = 0
-
 TOKEN_TELEGRAM_BOT = os.environ.get('TOKEN_TELEGRAM_BOT')
 REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = os.environ.get('REDIS_PORT')
@@ -22,6 +20,7 @@ REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
 PROXY_URL = os.environ.get('PROXY_URL')
 QUESTIONS_FILE = os.environ.get('QUESTIONS_FILE')
 
+NEW_QUESTION, SOLUTION_ATTEMPT = range(2)
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -39,50 +38,59 @@ def get_keyboard():
 
 def start(bot, update):
     update.message.reply_text('Начинаем, «Новый вопрос»!!!', reply_markup=get_keyboard())
-
-
-def handler_message(bot, update):
-    global question_count
-
-    if update.message.text == 'Новый вопрос':
-        filename = QUESTIONS_FILE
-        question = get_one_question(filename)
-        message_text = question['question']
-        answer_text = question['answer']
-        chat_id = update.message.chat.id
-        question_count = len(list(r.scan_iter(f'{chat_id}_question_*'))) + 1
-        set_key = f'{chat_id}_question_{question_count}'
-        set_value = json.dumps({
-            'question_count': question_count,
-            'question': message_text,
-            'answer': answer_text,
-        })
-        r.set(set_key, set_value)
-    else:
-        chat_id = update.message.chat.id
-        answer_user = update.message.text.lower()
-        question_count = len(list(r.scan_iter(f'{chat_id}_question_*')))
-        set_key = f'{chat_id}_question_{question_count}'
-        set_value = json.loads(r.get(set_key).decode())
-        answer = set_value['answer']
-        currently_answer = answer.rsplit('.')[0].rsplit('(')[0].strip().lower()
-
-        if currently_answer == answer_user:
-            message_text = 'Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»'
-            set_value['is_currently'] = 'true'
-        else:
-            message_text = 'Неправильно... Попробуешь ещё раз?'
-            set_value['is_currently'] = 'false'
-
-        set_value['answer_user'] = answer_user
-        set_value = json.dumps(set_value)
-        r.set(set_key, set_value)
-
-    update.message.reply_text(message_text)
+    return NEW_QUESTION
 
 
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
+
+
+def handle_new_question_request(bot, update):
+    question = get_one_question(QUESTIONS_FILE)
+    message_text = question['question']
+    answer_text = question['answer']
+    chat_id = update.message.chat.id
+    question_count = len(list(r.scan_iter(f'{chat_id}_question_*'))) + 1
+    set_key = f'{chat_id}_question_{question_count}'
+    set_value = json.dumps({
+        'question_count': question_count,
+        'question': message_text,
+        'answer': answer_text,
+    })
+    r.set(set_key, set_value)
+    update.message.reply_text(message_text)
+    return SOLUTION_ATTEMPT
+
+
+def handle_solution_attempt(bot, update):
+    chat_id = update.message.chat.id
+    answer_user = update.message.text.lower()
+    question_count = len(list(r.scan_iter(f'{chat_id}_question_*')))
+    set_key = f'{chat_id}_question_{question_count}'
+    set_value = json.loads(r.get(set_key).decode())
+    answer = set_value['answer']
+    currently_answer = answer.rsplit('.')[0].rsplit('(')[0].strip().lower()
+
+    if currently_answer == answer_user:
+        message_text = 'Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»'
+        set_value['is_currently'] = 'true'
+        result = NEW_QUESTION
+    else:
+        message_text = 'Неправильно... Попробуешь ещё раз?'
+        set_value['is_currently'] = 'false'
+        result = SOLUTION_ATTEMPT
+
+    set_value['answer_user'] = answer_user
+    set_value = json.dumps(set_value)
+    r.set(set_key, set_value)
+
+    update.message.reply_text(message_text)
+    return result
+
+
+def cancel(bot, update):
+    update.message.reply_text('cancel')
+    return ConversationHandler.END
 
 
 def main():
@@ -93,8 +101,17 @@ def main():
         updater = Updater(token=TOKEN_TELEGRAM_BOT)
 
     dp = updater.dispatcher
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(MessageHandler(Filters.text, handler_message))
+
+    conversation_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+        states={
+            NEW_QUESTION: [RegexHandler('^(Новый вопрос)$', handle_new_question_request)],
+            SOLUTION_ATTEMPT: [MessageHandler(Filters.text, handle_solution_attempt)]
+        },
+        fallbacks=[CommandHandler('cancel', cancel)]
+    )
+
+    dp.add_handler(conversation_handler)
     dp.add_error_handler(error)
 
     updater.start_polling()
