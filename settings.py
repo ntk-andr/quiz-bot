bc560a46a0b25863b8b3de8edbaeb53ef877da4b
Andrey Usov <nkt.andr@gmail.com>
2020-02-01 18:27:01 +0300

    fix example.env and settings.py

--- settings.py
+++ settings.py
@@ -3,10 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-TOKEN_TELEGRAM_BOT = os.environ.get('TOKEN_TELEGRAM_BOT')
+TELEGRAM_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 REDIS_HOST = os.environ.get('REDIS_HOST')
 REDIS_PORT = os.environ.get('REDIS_PORT')
 REDIS_PASSWORD = os.environ.get('REDIS_PASSWORD')
 PROXY_URL = os.environ.get('PROXY_URL')
 QUESTIONS_FILE = os.environ.get('QUESTIONS_FILE')
-TOKEN_VK_BOT = os.environ.get('TOKEN_VK_BOT')
+VK_TOKEN = os.environ.get('VK_TOKEN')
