b581e0d72c920520e831f240889c06d9705304de
Andrey Usov <nkt.andr@gmail.com>
2020-03-26 02:52:16 +0300

    fix questions.py

--- questions.py
+++ questions.py
@@ -1,17 +1,65 @@
 import random
 
+import redis
+
+
+def get_result(chat_id, answer_user, r):
+    """получаем результат"""
+    set_value = dict()
+    question_count = len(list(r.scan_iter(f'{chat_id}_question_*')))
+    set_key = f'{chat_id}_question_{question_count}'
+    answer = r.hget(set_key, 'answer').decode()
+    currently_answer = answer.rsplit('.')[0].rsplit('(')[0].strip().lower()
+    set_value['answer_user'] = answer_user
+    message_text = ''
+    if answer_user != 'Сдаться':
+        message_text = 'Неправильно... Попробуешь ещё раз?'
+        set_value['is_currently'] = 'false'
+    elif answer_user == currently_answer:
+        message_text = 'Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»'
+        set_value['is_currently'] = 'true'
+    save_in_redis(set_key, set_value, r)
+    return {
+        'set_key': set_key,
+        'set_value': set_value,
+        'message_text': message_text
+    }
+
+
+def get_message_for_new_question(chat_id, question, r):
+    message_text = question['question']
+    answer_text = question['answer']
+    question_count = len(list(r.scan_iter(f'{chat_id}_question_*'))) + 1
+    set_key = f'{chat_id}_question_{question_count}'
+    set_value = {
+        'question_count': question_count,
+        'question': message_text,
+        'answer': answer_text,
+    }
+    save_in_redis(set_key, set_value, r)
+    print('Правильный ответ', answer_text)
+    return message_text
+
+
+def get_message_for_surrender(chat_id, r):
+    question_count = len(list(r.scan_iter(f'{chat_id}_question_*')))
+    set_key = f'{chat_id}_question_{question_count}'
+    answer = r.hget(set_key, 'answer').decode()
+    return f"Правильный ответ: {answer}\nЧтобы продолжить нажми «Новый вопрос»"
+
 
 def get_text(text: str) -> str:
-    return text.replace('\n', ' ')[text.find(':') + 1:].strip()
+    symbol_position = text.find(':') + 1
+    return text.replace('\n', ' ')[symbol_position:].strip()
 
 
-def read_file(filename: str, encoding='KOI8-R') -> list:
+def read_file(filename: str, encoding='KOI8-R') -> str:
     with open(filename, 'r', encoding=encoding) as file:
-        return file.read().split('\n\n\n')
+        return file.read()
 
 
-def get_questions(filename: str) -> dict:
-    quiz = read_file(filename)
+def get_questions(text: str) -> dict:
+    quiz = text.split('\n\n\n')
     answer = question = ''
     questions = dict()
     for quiz_question in quiz:
@@ -27,11 +75,17 @@ def get_questions(filename: str) -> dict:
     return questions
 
 
-def get_one_question(filename: str) -> dict:
-    questions = list(get_questions(filename).items())
-    choice = random.choice(questions)
+def get_question(text: str) -> dict:
+    questions = list(get_questions(text).items())
+    question, answer = random.choice(questions)
 
     return {
-        'question': choice[0],
-        'answer': choice[1]
+        'question': question,
+        'answer': answer
     }
+
+
+def save_in_redis(set_key: str, set_value: dict, r: redis):
+    for key in set_value:
+        value = str(set_value[key])
+        r.hset(set_key, key, value)
